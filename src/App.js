ef221a47ce84d65fa83f97398b2c5fb14f4f2c9c
graditos <graditos@proton.me>
2025-02-26 00:47:41 +0100

    intento 3

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [error, setError] = useState(null);
 
   // URL del backend (actualizar con la URL de tu Replit)
-  const BACKEND_URL = "https://api.render.com/deploy/srv-cuv53elds78s73fnq7kg?key=4BpoAhTz3Kg";
+  const BACKEND_URL = "https://backend-tolc.onrender.com";
 
   const handleInputChange = (e) => {
     setInputText(e.target.value);
